9e79b653da60d4099cfb883e052fefdd8d3ec831
FrtttZ <85700060+FrtttZ@users.noreply.github.com>
2026-05-04 18:56:07 +0800

    Create function that takes numbers and the operator
    Function evaluates operator then call needed function

--- script.js
+++ script.js
@@ -14,4 +14,16 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
+//createe function
+//take numbers and operators
+//call function depends on the operator
+function operate(num1, num2, operate){
+    switch(operate){
+        case '+': return add(num1, num2);
+    }
+}
 
+
+let firstNum = null;
+let secondNum = null;
+let operator = null;
