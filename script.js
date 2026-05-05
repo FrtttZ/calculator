a60c4a8928b2d25cd2bfebdc1dc9a3239b733855
FrtttZ <85700060+FrtttZ@users.noreply.github.com>
2026-05-05 11:33:36 +0800

    Create function for operations and revised getNumber

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const display = document.querySelector('#display');
+
 let firstNum = null;
 let secondNum = null;
 let operator = null;
+let toDisplay = null;
 
-let display = document.querySelector('#display');
+
 
 function add(num1, num2){
     return num1 + num2;
@@ -20,13 +23,23 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-function operate(num1, num2, operate){
-    switch(operate){
-        case '+': return add(num1, num2);
-        case '-': return subtract(num1, num2);
-        case '*': return multiply(num1, num2);
-        case '/': return divide(num1, num2);
+function operate(x, y, op){
+
+    num1 = parseInt(x);
+    num2 = parseInt(y)
+    console.log('firstNum: ' + num1);
+    console.log(op);
+    console.log(num2);
+
+    switch(op){
+        case '+': return add(num1, num2); break;
+        case '-': return subtract(num1, num2); break;
+        case '*': return multiply(num1, num2); break;
+        case '/': return divide(num1, num2); break;
+        default: return 'NO';
     }
+
+  
 }
 
 function getNum(){
@@ -37,18 +50,79 @@ function getNum(){
 
 
         if (event.target == event.currentTarget) return
-        
-        if(!firstNum){
-            firstNum = event.target.id;
+
+
+        if (!operator){
+                    
+            if(!firstNum){
+                firstNum = event.target.id;
+            }
+            else if(firstNum){
+                firstNum += event.target.id;
+            }
+        }else{
+            if(!secondNum){
+                secondNum = event.target.id;
+            }
+            else if(secondNum){
+                secondNum += event.target.id;
+            }
+
         }
-        else if(firstNum){
-            firstNum += event.target.id;
+
+        if (!toDisplay) {
+            toDisplay = event.target.id
+        }else{
+            toDisplay += event.target.id;
         }
-        display.placeholder = firstNum;
+
+        display.placeholder = toDisplay;
+    
     })
+
+    // toDisplay += firstNum;
+    // display.placeholder = toDisplay
 }
 
+//add number to the second variable if there are still no operator
+
+function getOperator(){
+    let operatorArea = document.querySelector('#operators');
+
+    operatorArea.addEventListener('click', (event) => {
+        if (event.target == event.currentTarget) return
+        let clickedOperator = event.target.id;
+
+        switch(clickedOperator){
+            case 'plus':
+                operator = '+';
+                break;
+            case 'minus':
+                operator = '-';
+                break;
+            case 'multiply':
+                operator = '*';
+                break;
+            case 'divide':
+                operator = '/';
+                break;
+            case 'clear':
+                break;
+            case 'equals':
+                toDisplay = operate(firstNum,secondNum,operator);
+                console.log(toDisplay);
+        }
+
+        if(event.target.id != 'equals') toDisplay+= operator;  
+        display.placeholder = toDisplay;
+    })
+}
+//operator function: what should it include?
+//operator that change the variable and add it to the screen;
+//if = is clicked use the function needed
 
 
+
+getOperator();
 getNum();
 
